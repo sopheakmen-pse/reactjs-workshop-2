8c0ed1261b49680492c330a394f2a37522827eae
sopheakmen-pse <sopheak.men@pse.ngo>
2024-07-10 14:43:00 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1 className="gradient-heading">To-Do List</h1>
+
+      <form className="form-add-task">
+        <input type="text" placeholder="Add a task" />
+        <button className="btn btn-add" type="submit">
+          Add
+        </button>
+      </form>
+
+      <div className="todo-list">
+        <div className="todo-list-item">
+          <input type="checkbox" id="task1" />
+          <label for="task1">Task 1</label>
+          <button className="btn btn-delete">Delete</button>
+        </div>
+
+        <div className="todo-list-item">
+          <input type="checkbox" id="task2" />
+          <label for="task2">Task 2</label>
+          <button className="btn btn-delete">Delete</button>
+        </div>
+
+        <div className="todo-list-item">
+          <input type="checkbox" id="task3" />
+          <label for="task3">Task 3</label>
+          <button className="btn btn-delete">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/heading.jsx
@@ -0,0 +1,5 @@
+function Heading() {
+  return <h1 className="gradient-heading">To-Do List</h1>;
+}
+
+export default Heading;
